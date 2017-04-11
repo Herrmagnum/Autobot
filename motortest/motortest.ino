9ebbd37d88eb27426ef75f4d05fff7f561d3567c
pettuv-3 <petter.tuvehed@hotmail.com>
2017-04-11 15:43:07 +0200

    Add files via upload

---
+++ motortest/motortest.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+int OP=9;
+pinMode(OP, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+
+  digitalWrite(OP, HIGH); 
+  delay(1) ;
+  digitalWrite(OP, LOW);
+  delay(1000);
+
+  
+  digitalWrite(OP, HIGH); 
+  delay(1.5) ;
+  digitalWrite(OP, LOW);
+  delay(1000);
+}
